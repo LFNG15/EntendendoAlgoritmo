9d1f4f799874e8dee6872c904b088c0660a65fdd
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-03-11 17:44:00 -0300

    Update Ordenação por seleção

--- 2.Ordenação por seleção/selection_sorting.cpp
+++ 2.Ordenação por seleção/selection_sorting.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-int buscamenor(vector<int>% arr)
+int buscamenor(vector<int>& arr)
 {
     int menor = arr[0];
     int menor_indice = 0;
@@ -15,4 +15,22 @@ int buscamenor(vector<int>% arr)
         }
     }
     return menor_indice;
+}
+
+vector<int> ordenando_por_selecao(vector<int>& arr) {
+    vector<int> novo_arr;
+    for (int i = 0; i < arr.size(); i++){
+        int menor = buscamenor(arr);
+        novo_arr.push_back(arr[menor]);
+        arr.erase(arr.begin() + menor);
+    }
+    return novo_arr;
+}
+
+int main() {
+    vector<int> arr = {5, 3, 6, 2, 10};
+    vector<int> sorted_arr = ordenando_por_selecao(arr);
+    for (int i = 0; i < sorted_arr.size(); i++) {
+        cout << sorted_arr[i] << " ";
+    }
 }
